b65c67e83727bb414644dec9db7c811e5c24f110
NikzadE <arshad.nikzad@gmail.com>
2018-08-21 13:29:28 -0400

    Got the first part working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,49 +1,64 @@
 $(document).ready(function(){
 
-    var trainData = new Firebase ("https://train-database.firebaseio.com/");
+    var config = {
+        apiKey: "AIzaSyBJ-_F_fYAdV7GInyGoOJLw7fWCb1S3Tmc",
+        authDomain: "trainschedule-22eed.firebaseapp.com",
+        databaseURL: "https://trainschedule-22eed.firebaseio.com",
+        projectId: "trainschedule-22eed",
+        storageBucket: "trainschedule-22eed.appspot.com",
+        messagingSenderId: "627171314372"
+      };
+      firebase.initializeApp(config);
+      console.log(firebase);
+
+    var database = firebase.database().ref();
     
     $("#addTrainButton").on("click", function(){
-
+      
         var trainName = $("#trainNameInput").val().trim();
-        var lineName = $("#lineInput").val().trim();
         var destination = $("#destinationInput").val().trim();
         var trainTimeInput = moment($("#trainTimeInput").val().trim(), "HH:mm").subtract(10, "years").format("X");;
         var frequencyInput = $("#frequencyInput").val().trim();
 
         var newTrain = {
             name: trainName,
-            line: lineName,
             destination: destination,
             trainTime: trainTimeInput,
             frequency: frequencyInput,
         }
 
-        trainData.push(newTrain);
+        database.push(newTrain);
 
         $("#trainNameInput").val("");
-        $("#lineInput").val("");
         $("#destinationInput").val("");
         $("#trainInput").val("");
         $("#frequencyInput").val("");
 
-        return false;
+       
 
     });
+ //display firebase objects on page
+    database.on("value", displayData);
+    function displayData (data) {
+        console.log(data.val());
 
-    trainData.on("child_added", function(childSupport, prevChildkey){
+    }
+    
 
-        var firebaseName = childSnapshot.val().name;
-        var firebaseLine = childSnapshot.val().line;
-        var firebaseDestination = childSnapshot.val().destination;
-        var firebaseTrainTimeInput = childSnapshot.val().trainTime;
-        var firebaseFrequency = childSnapshot.val().frequency;
+        var firebaseName = data.val();
+        var keys = Object.keys(firebaseName);
+        for (var i = 0; i<keys.length; i++);
+        var k = keys [i];
+        var firebaseDestination = firebaseName [k];
+        var firebaseTrainTimeInput = data.val().trainTime;
+        var firebaseFrequency = data.val().frequency;
 
-        var diffTime = moment().diff(moment.unix(firebasetrainTimeInput), "minute");
+      
         var timeRemainder = moment().diff(moment.unix(firebaseTrainTimeInput), "minutes")%firebaseFrequency;
         var minutes = firebaseFrequency - timeRemainder;
 
         var nextTrainArrival = moment().add(minutes, "m").format("hh:mm A");
-        $("#trainTable > tbody").append("<tr><td>"+ firebaseName + "</td> <td>" + firebaseLine + "</td><td>" + firebaseDestination+ "</td><td>" + firebaseFrequency + "mins" + "</td><td>" + nextTrainArrival + "</td><td>" + minutes + "</td> </tr>");
+        $("#trainTable > tbody").append("<tr><td>"+ firebaseName + "</td><td>" + firebaseDestination+ "</td><td>" + firebaseFrequency + "mins" + "</td><td>" + nextTrainArrival + "</td><td>" + minutes + "</td> </tr>");
 
-    });
+    ;
 });
